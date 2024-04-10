930f4cf8569081472b3fdaae7f693aa405f621d3
Daniel Amaral <silva.daniel86@gmail.com>
2024-04-10 17:16:22 -0300

    🐛 fix(auth): try to fix undefined error in session

--- src/components/shared/auth/AuthButton.tsx
+++ src/components/shared/auth/AuthButton.tsx
@@ -6,11 +6,12 @@ import { LoginButton, UserButton } from '@/components/shared/auth'
 export default async function AuthButton() {
   try {
     const session = await getServerAuthSession()
-    const user = session
+    const user = session?.user
       ? await api.user.getUserByUsername.query({
           username: session.user.username,
         })
       : null
+    console.log(session, user)
 
     return user ? <UserButton user={user} /> : <LoginButton />
   } catch (error) {
