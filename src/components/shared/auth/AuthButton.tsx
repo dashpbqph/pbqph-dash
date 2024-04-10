d75d4d044100144f1d3615edb5039d343853162f
Daniel Amaral <silva.daniel86@gmail.com>
2024-04-10 17:33:04 -0300

    temp fix to emulate user in auth

--- src/components/shared/auth/AuthButton.tsx
+++ src/components/shared/auth/AuthButton.tsx
@@ -1,17 +1,29 @@
 // import { getServerAuthSession } from '@/server/auth'
-import { api } from '@/trpc/server'
+// import { api } from '@/trpc/server'
+import { UserRole } from '@prisma/client'
 
 import { LoginButton, UserButton } from '@/components/shared/auth'
 
 export default async function AuthButton() {
   try {
     // const session = await getServerAuthSession()
-    const user = await api.user.getUserByUsername.query({
-      username: 'super.admin',
-    })
-    console.log(user)
+    // const user = await api.user.getUserByUsername.query({
+    //   username: 'super.admin',
+    // })
+    // console.log(user)
 
-    return user ? <UserButton user={user} /> : <LoginButton />
+    const fakeUser = {
+      avatar: 'https://avatars.githubusercontent.com/u/3040235',
+      username: 'test.user',
+      name: 'Test User',
+      firstName: 'Test',
+      lastName: 'User',
+      email: 'test.user@gmail.com',
+      role: UserRole.STAFF,
+      company: undefined,
+    }
+
+    return <UserButton user={fakeUser} />
   } catch (error) {
     console.error(error)
     return <LoginButton />
