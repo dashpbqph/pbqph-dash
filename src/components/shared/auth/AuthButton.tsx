d38bc122986733a065d3a50799d41100dfdcbe90
Daniel Amaral <silva.daniel86@gmail.com>
2024-04-10 23:15:48 -0300

    fix bug

--- src/components/shared/auth/AuthButton.tsx
+++ src/components/shared/auth/AuthButton.tsx
@@ -1,29 +1,19 @@
-// import { getServerAuthSession } from '@/server/auth'
-// import { api } from '@/trpc/server'
-import { UserRole } from '@prisma/client'
+import { getServerAuthSession } from '@/server/auth'
+import { api } from '@/trpc/server'
 
 import { LoginButton, UserButton } from '@/components/shared/auth'
 
 export default async function AuthButton() {
   try {
-    // const session = await getServerAuthSession()
-    // const user = await api.user.getUserByUsername.query({
-    //   username: 'super.admin',
-    // })
-    // console.log(user)
-
-    const fakeUser = {
-      avatar: 'https://avatars.githubusercontent.com/u/3040235',
-      username: 'test.user',
-      name: 'Test User',
-      firstName: 'Test',
-      lastName: 'User',
-      email: 'test.user@gmail.com',
-      role: UserRole.STAFF,
-      company: undefined,
-    }
-
-    return <UserButton user={fakeUser} />
+    const session = await getServerAuthSession()
+    console.log(session)
+    const user = session
+      ? await api.user.getUserByUsername.query({
+          username: 'super.admin',
+        })
+      : null
+    console.log(user)
+    return user ? <UserButton user={user} /> : <LoginButton />
   } catch (error) {
     console.error(error)
     return <LoginButton />
