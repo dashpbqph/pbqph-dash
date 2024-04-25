9dc8bdce8bc7bb54e5f200f988f7d19d74aac24c
Daniel Amaral <silva.daniel86@gmail.com>
2024-04-25 17:41:17 -0300

    test

--- src/components/shared/auth/AuthButton.tsx
+++ src/components/shared/auth/AuthButton.tsx
@@ -1,5 +1,5 @@
 import { getServerAuthSession } from '@/server/auth'
-import { api } from '@/trpc/server'
+import { db } from '@/server/db'
 
 import { LoginButton, UserButton } from '@/components/shared/auth'
 
@@ -9,11 +9,42 @@ export default async function AuthButton() {
 
     if (!session) throw new Error('No session')
 
-    const user = await api.user.getUserByUsername.query({
-      username: session.user.username,
+    const user = await db.user.findFirst({
+      where: {
+        username: session.user.username,
+      },
+      include: {
+        role: {
+          select: {
+            role: true,
+          },
+        },
+        company: {
+          select: {
+            name: true,
+          },
+        },
+      },
     })
 
-    return <UserButton user={user} />
+    if (!user) throw new Error('User not found')
+
+    return (
+      <UserButton
+        user={{
+          avatar: user.image,
+          username: user.username,
+          name: user.lastName
+            ? `${user.firstName} ${user.lastName}`
+            : user.firstName,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          role: user.role?.role,
+          company: user.company?.name,
+        }}
+      />
+    )
   } catch (error) {
     if ((error as Error)?.message !== 'No session') {
       console.error(error)
